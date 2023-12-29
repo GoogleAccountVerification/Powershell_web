b3ae4920c827cabf8bcca46f58e26d0c82d0252f
Anitsingh9599 <90022606+Anitsingh9599@users.noreply.github.com>
2023-12-29 08:47:37 +0530

    Delete run_powershell.py

--- run_powershell.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-def execute_powershell(command):
-    result = subprocess.run(["powershell", "-Command", command], capture_output=True, text=True)
-    return result.stdout
-
-if __name__ == "__main__":
-    command_output = execute_powershell("whoami")
-    print(command_output)
